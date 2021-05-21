56f3dbc2982be732b31452fa7fa6f2cf96390114
Alfred DSouza <dsouzaalfred@gmail.com>
2021-05-21 15:57:19 +0100

    add title and change font size to rem

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ export default function App() {
   const { state, nowPlaying, isWon, isDraw, winningCells, reset, handleClick } = useTicTacToe();
   return (
     <div className={styles.app}>
+      <h1>Tic-Tac-Toe</h1>
       <div className={styles.wrapper}>
         <Header isWon={isWon} isDraw={isDraw} nowPlaying={nowPlaying} reset={reset} />
         <Grid handleClick={handleClick} gameState={state} winningCells={winningCells} isWon={isWon} />
